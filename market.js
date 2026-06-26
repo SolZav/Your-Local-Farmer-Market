e678289cb197facab2a2350328406aef5bbe443c
SolZav <mllinasm@asu.edu>
2026-06-26 11:16:29 -0400

    Game play function
    created js for game play, updated html and css to improve game play

--- market.js
+++ market.js
@@ -72,4 +72,56 @@ btn1.addEventListener('click', () => {
 
 //game play
 
+//create function to get pseudo random number
+function getRandomNumber(min, max) {
+   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//create game play function
+function gamePlay(e){
+  //prevents default from submission
+  e.preventDefault();
+
+  //declare variables
+  let gameErrorMessg = document.querySelector("#gamePlay span"); //error msg to be displayed
+  let gameOutput = document.querySelector("#gameOutput"); //game result message
+  let inputNumber = document.getElementById("numGuess"); //number input by user
+  let randNumber = getRandomNumber(1, 10); //creates random number to compare with user input
+ 
+  let num = parseInt(inputNumber.value); //make the number input from a string type to a integer type
+
+   //reset the game after every play
+  gameOutput.innerHTML = ""; //clear output
+  inputNumber.value = ""; //clear input
+  gameErrorMessg.classList.add("message"); //hide error message
+  inputNumber.classList.remove("errorInput"); //remove red border from input 
+
+  console.log(num);
+  
+  //use boolean to track validity
+  let isValid = true;
+
+  //validate that the input number is between 1 and 10 or if there's no input
+  if(isNaN(num) || num < 1 || num > 10){
+    inputNumber.classList.add("errorInput");
+    gameErrorMessg.classList.remove("message");
+    gameOutput = "";
+    isValid = false;
+  }
+
+  //if number is valid check if user wins or lose and display appropriate message
+  if(isValid){
+    if(num == randNumber){
+      gameOutput.innerHTML = "You win! Your number: " + num + " Winning number: " + randNumber;
+    } else {
+      gameOutput.innerHTML = "You lose! Your number: " + num + " Winning number: " + randNumber + " Try again!";
+    } 
+  }
+}
+
+//add event listener to run the game play function
+document.getElementById("guessGame").addEventListener("click", gamePlay);
+
+
+
 //form validation
